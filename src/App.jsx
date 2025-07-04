96b02ece5ef609611cdcfbbcf45cb530b94adb80
Varshacustom4321 <varashacustomcomputing@gmail.com>
2025-07-04 20:53:58 +0530

    minor change

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Header from './common/Header'
-import Home from './pages/Home'
-import RoutesUrls from './common/Routesurls'
-import './css/App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './common/Header';
+import Home from './pages/Home';
+import RoutesUrls from './common/Routesurls';
+import './css/App.css';
 
 function App() {
   return (
     <div className='mainapp'>
-      <Router>
+      {/* Updated Router with basename for GitHub Pages */}
+      <Router basename="/varsha-fe">
         <Header />
         <Routes>
+          {/* Home route */}
+          <Route path="/" element={<Home />} />
+          
+          {/* Dynamically rendered routes from RoutesUrls */}
           {RoutesUrls}
-        </Routes> 
+          
+          {/* Optional: 404 catch-all route */}
+          <Route path="*" element={<div>Page Not Found</div>} />
+        </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
